7772fc3ca59cd60d8396e5c52d53c25cf7f6cdad
Tania <tanner.shirkey@gmail.com>
2024-03-14 16:47:57 -0400

    localStorage Page Load
    Added functionality for the page to create previous searches from localStorage

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,34 @@ const weatherText = document.getElementById("weather-txt");
 const searchButton = document.getElementById("search-button");
 
 const searchCity = (e) => {
+  // Function to search API for the user inputted city.
   e.preventDefault();
   const cityList = JSON.parse(localStorage.getItem("city-list")); // Loads localStorage as an array
-  let inputText = weatherText.value; // Saving value to let allows re-execution of function
-  weatherText.value = ""; // Clears input field
+  const inputText = weatherText.value; // Saving value to let allows re-execution of function
+  weatherText.value = "";
   cityList.push(inputText);
   localStorage.setItem("city-list", JSON.stringify(cityList));
-  // Then execute function to create p elements from localStorage
 
+  // Create new elements based on user input
   let HTMLText = `
-  The weather in ${inputText} is nice!`; // Placeholder text, replace with creating saved search buttons
-  let paragraphEl = document.createElement("p"); // Turn this element creation into a for loop
+    The weather in ${inputText} is nice!`;
+  let paragraphEl = document.createElement("p");
   paragraphEl.innerText = HTMLText;
   document.body.appendChild(paragraphEl);
 };
 
+const createPreviousSearch = () => {
+  // Function to create elements from localStorage on page load
+  const cityList = JSON.parse(localStorage.getItem("city-list"));
+
+  for (let i = 0; i < cityList.length; i++) {
+    let HTMLText = `
+      The weather in ${cityList[i]} is nice!`; // Placeholder text, replace with creating saved search buttons
+    let paragraphEl = document.createElement("p");
+    paragraphEl.innerText = HTMLText;
+    document.body.appendChild(paragraphEl);
+  }
+};
+
+createPreviousSearch();
 searchButton.addEventListener("click", searchCity);
