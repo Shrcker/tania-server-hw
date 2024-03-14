f9f5614ee104b55343291e92e6d004b43414ee11
Tania <tanner.shirkey@gmail.com>
2024-03-14 13:38:44 -0400

    HTML Form
    Added interactive HTML form used to stage user weather search

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,1 @@
-// const to access API url
-const requestURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={0ec149fc41b8add03505b09f347f63e2}`;
-// declaring placeholder variables
-var lat;
-var lon;
 console.log("Hello World");
