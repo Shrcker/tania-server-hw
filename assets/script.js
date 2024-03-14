0c6966ddf2c0f37f0840bd4473e3b3ef8358c5df
Tania <tanner.shirkey@gmail.com>
2024-03-14 15:49:27 -0400

    Trying to figure out how to use localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,17 @@
 const weatherText = document.getElementById("weather-txt");
 const searchButton = document.getElementById("search-button");
+const cityList = [];
 
-const searchCity = (e) => {
+const searchCity = (input) => {
+  //e.preventDefault();
+  //const setCity = localStorage.setItem("city-list", cityList.push(input));
   let inputText = weatherText.value;
-  e.preventDefault();
   let HTMLText = `
-      The weather in ${inputText} is nice!`;
-  let paragraphEl = document.createElement("p");
+    The weather in ${inputText} is nice!`;
+  let paragraphEl = document.createElement("p"); // Turn this element creation into a for loop
   paragraphEl.innerText = HTMLText;
   document.body.appendChild(paragraphEl);
   weatherText.value = "";
 };
 
-searchButton.addEventListener("click", searchCity);
+searchButton.addEventListener("click", searchCity(weatherText.value));
