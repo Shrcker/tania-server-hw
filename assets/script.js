281b6479ef04971dad82c61cd8fbef0d04c06223
Tania <tanner.shirkey@gmail.com>
2024-03-14 14:04:17 -0400

    JS Button
    JS now listens to search input with an eventListener, does not actually search right now

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const weatherText = document.getElementById("weather-txt");
+const searchButton = document.getElementById("search-button");
+
+const searchCity = (e) => {
+  let inputText = weatherText.value;
+  e.preventDefault();
+  let HTMLText = `
+      The weather in ${inputText} is nice!`;
+  let paragraphEl = document.createElement("p");
+  paragraphEl.innerText = HTMLText;
+  document.body.appendChild(paragraphEl);
+  weatherText.value = "";
+};
+
+searchButton.addEventListener("click", searchCity);
