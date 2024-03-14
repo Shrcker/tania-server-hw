2471b287ba8a10c0feaa82d5398263a76cdde312
Tania <tanner.shirkey@gmail.com>
2024-03-14 16:32:12 -0400

    Enabled localStorage
    User input is now saved to localStorage reliably

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,20 @@
 const weatherText = document.getElementById("weather-txt");
 const searchButton = document.getElementById("search-button");
-const cityList = [];
 
-const searchCity = (input) => {
-  //e.preventDefault();
-  //const setCity = localStorage.setItem("city-list", cityList.push(input));
-  let inputText = weatherText.value;
+const searchCity = (e) => {
+  e.preventDefault();
+  const cityList = JSON.parse(localStorage.getItem("city-list")); // Loads localStorage as an array
+  let inputText = weatherText.value; // Saving value to let allows re-execution of function
+  weatherText.value = ""; // Clears input field
+  cityList.push(inputText);
+  localStorage.setItem("city-list", JSON.stringify(cityList));
+  // Then execute function to create p elements from localStorage
+
   let HTMLText = `
-    The weather in ${inputText} is nice!`;
+  The weather in ${inputText} is nice!`; // Placeholder text, replace with creating saved search buttons
   let paragraphEl = document.createElement("p"); // Turn this element creation into a for loop
   paragraphEl.innerText = HTMLText;
   document.body.appendChild(paragraphEl);
-  weatherText.value = "";
 };
 
-searchButton.addEventListener("click", searchCity(weatherText.value));
+searchButton.addEventListener("click", searchCity);
