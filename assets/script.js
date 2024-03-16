326a27a38aa8b78db684590b00d26a4d3e802938
Tania <tanner.shirkey@gmail.com>
2024-03-16 17:57:03 -0400

    Fixed miscellaneous bugs
    Fixed issues with localStorage not saving as an array and cleaned up some code

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,16 @@
-const weatherText = document.getElementById("weather-txt");
+const searchText = document.getElementById("weather-txt");
 const searchButton = document.getElementById("search-button");
+const savedCities = JSON.parse(localStorage.getItem("city-list"));
+let cityList = [];
 
 const searchCity = (e) => {
   // Function to search API for the user inputted city.
   e.preventDefault();
-  const cityList = JSON.parse(localStorage.getItem("city-list")); // Loads localStorage as an array
-  const inputText = weatherText.value; // Saving value to let allows re-execution of function
-  weatherText.value = "";
+  // const cityList = JSON.parse(localStorage.getItem("city-list")); // Loads localStorage as an array
+  const inputText = searchText.value; // Saving value to let allows re-execution of function
   cityList.push(inputText);
   localStorage.setItem("city-list", JSON.stringify(cityList));
+  searchText.value = "";
 
   // Create new elements based on user input
   let HTMLText = `
@@ -20,16 +22,16 @@ const searchCity = (e) => {
 
 const createPreviousSearch = () => {
   // Function to create elements from localStorage on page load
-  const cityList = JSON.parse(localStorage.getItem("city-list"));
-
-  for (let i = 0; i < cityList.length; i++) {
+  for (let i = 0; i < savedCities.length; i++) {
     let HTMLText = `
-      The weather in ${cityList[i]} is nice!`; // Placeholder text, replace with creating saved search buttons
+      The weather in ${savedCities[i]} is nice!`; // Placeholder text, replace with creating saved search buttons
     let paragraphEl = document.createElement("p");
     paragraphEl.innerText = HTMLText;
     document.body.appendChild(paragraphEl);
   }
 };
 
-createPreviousSearch();
+if (savedCities) {
+  createPreviousSearch();
+}
 searchButton.addEventListener("click", searchCity);
